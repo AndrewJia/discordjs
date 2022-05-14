2b557fd1fb1a7f99b645a19c15e8234ad2100c1e
Andrew Jia <andrew.jia.2013@gmail.com>
2022-05-13 17:42:12 -0700

    Update clear.js
    print dice count in easy to read format

--- commands/clear.js
+++ commands/clear.js
@@ -9,14 +9,22 @@ module.exports = {
         count = [0, 0, 0, 0, 0, 0];
         for(i = 1; i < global.lul.length; i+=2) {
             for(j = 0; j < global.lul[i].length; j++) {
-                console.log(global.lul[i][j]);
+                //console.log(global.lul[i][j]);
                 count[global.lul[i][j]-1]++;
             }
         }
 
         console.log(count);
-        await interaction.reply("array was "+global.lul+"\ntotal dice counts "+count);
-        //await interaction.channel.send(count);
+        
+        await interaction.reply("array was "+global.lul);
+
+        countString = "";
+        for(val = 1; val <= 6; val++) {
+            countString += (count[val-1]) + " " + val + "'s\n";
+        }
+        await interaction.channel.send(countString);
+
+        //clears stored dice rolls
         global.lul = Array();
 	},
 };
