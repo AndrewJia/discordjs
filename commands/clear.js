09c652edaf2625ef7ef30df23b9e8449d73cd40d
Andrew Jia <andrew.jia.2013@gmail.com>
2022-05-14 22:18:53 -0700

    Update clear.js
    doesn't message, just clears dice

--- commands/clear.js
+++ commands/clear.js
@@ -4,22 +4,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('clear')
 		.setDescription('clears stored dice rolls'),
-	async execute(interaction) {
-        //total dice count
-        count = [0, 0, 0, 0, 0, 0];
-        for(i = 1; i < global.rolls.length; i+=2) {
-            for(j = 0; j < global.rolls[i].length; j++) {
-                count[global.rolls[i][j]-1]++;
-            }
-        }
-        
-        await interaction.reply("array was "+global.rolls);
 
-        countString = "";
-        for(val = 1; val <= 6; val++) {
-            countString += (count[val-1]) + " " + val + "'s\n";
-        }
-        await interaction.channel.send(countString);
+	async execute(interaction) {
+        await interaction.reply("cleared all dice rolls");
 
         //clears stored dice rolls and bet
         global.rolls = Array();
